135926fe85c11d1bf3c73a02a7b7b12651f109ec
Damian chabior <damianchabior@MacBook-Air-Damian.local>
2026-09-24 00:45:37 +0200

    feat: add short URL model

--- shortener/models.py
+++ shortener/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+
+class ShortURL(models.Model):
+    url = models.URLField(max_length=2048)
+    code = models.CharField(max_length=8, unique=True)
