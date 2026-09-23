e95e816c610da1fa31899f8f2865376eb1732a8a
Damian chabior <damianchabior@MacBook-Air-Damian.local>
2026-09-23 23:50:00 +0200

    feat: add Django skeleton and Docker Compose setup

--- Dockerfile
+++ Dockerfile
@@ -8,4 +8,6 @@ WORKDIR /app
 COPY requirements.txt .
 RUN python -m pip install --no-cache-dir -r requirements.txt
 
-CMD ["python", "--version"]
+COPY . .
+
+CMD ["python", "manage.py", "runserver", "0.0.0.0:8000"]
---
+++ shortener/views.py
@@ -0,0 +1,10 @@
+from rest_framework.decorators import api_view, renderer_classes
+from rest_framework.renderers import JSONRenderer
+from rest_framework.request import Request
+from rest_framework.response import Response
+
+
+@api_view(['GET'])
+@renderer_classes([JSONRenderer])
+def hello_world(request: Request) -> Response:
+    return Response({'message': 'Hello world'})
