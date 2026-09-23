faee22c8a29b1688b5ecedc2de59b6bc949f8360
Damian chabior <damianchabior@MacBook-Air-Damian.local>
2026-09-23 23:50:00 +0200

    feat: add Django skeleton and Docker Compose setup

---
+++ shortener/tests.py
@@ -0,0 +1,16 @@
+from django.test import SimpleTestCase
+from django.urls import reverse
+
+
+class HelloWorldTests(SimpleTestCase):
+    def test_get_returns_hello_world_json(self) -> None:
+        response = self.client.get(reverse('hello-world'))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response['Content-Type'], 'application/json')
+        self.assertEqual(response.json(), {'message': 'Hello world'})
+
+    def test_post_is_not_allowed(self) -> None:
+        response = self.client.post(reverse('hello-world'))
+
+        self.assertEqual(response.status_code, 405)
